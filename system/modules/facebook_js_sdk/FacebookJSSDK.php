da5394f4615e3275fc128f0be554a7c5c9bb9b93
fritzmg <email@spikx.net>
2017-04-10 14:35:40 +0200

    provided functionality to disable integration

--- system/modules/facebook_js_sdk/FacebookJSSDK.php
+++ system/modules/facebook_js_sdk/FacebookJSSDK.php
@@ -13,21 +13,60 @@
  */
 
 
-class FacebookJSSDK extends \Frontend
+class FacebookJSSDK
 {
+
+    /**
+     * Force disable in back end
+     * @var boolean
+     */
+    protected static $blnDisable = false;
+
+
+    /**
+     * Static function to force disable in the back end.
+     *
+     * @return void
+     */
+    public static function disable()
+    {
+        self::$blnDisable = true;
+    }
+
+
+    /**
+     * outputFrontendTemplate/outputBackendTemplate Hook
+     *
+     * @param string $strBuffer
+     * @param string $strTemplate
+     *
+     * @return string
+     */
     public function inject( $strBuffer, $strTemplate )
     {
+        // check if disabled
+        if( self::$blnDisable )
+        {
+            return $strBuffer;
+        }
+
         // check for appId and version in config
         if( !$GLOBALS['TL_CONFIG']['fb_app_id'] || !$GLOBALS['TL_CONFIG']['fb_app_version'] )
+        {
             return $strBuffer;
+        }
 
         // check for frontend or backend template
-        if( stripos( $strTemplate, 'fe_') === false && stripos( $strTemplate, 'be_main') === false )
+        if( stripos($strTemplate, 'fe_') === false && stripos($strTemplate, 'be_main') === false )
+        {
             return $strBuffer;
+        }
 
         // check for <body
         if( stripos( $strBuffer, '<body') === false )
+        {
             return $strBuffer;
+        }
 
         // determine language string
         $lang = $GLOBALS['TL_LANGUAGE'];
