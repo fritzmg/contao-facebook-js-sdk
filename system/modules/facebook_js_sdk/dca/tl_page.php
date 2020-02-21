943c21505ee2bf6dd0a8e1e2eacf6a020ed1116d
David Molineus <david.molineus@netzmacht.de>
2020-02-21 09:10:41 +0100

    Restore compatibility with Contao 4.9
    Contao 4.9 introduces new palette rootfallback for root pages with activated fallback

--- system/modules/facebook_js_sdk/dca/tl_page.php
+++ system/modules/facebook_js_sdk/dca/tl_page.php
@@ -4,7 +4,7 @@
  * Contao Open Source CMS
  *
  * simple extension to automatically integrate the Facebook JavaScript SDK in the Contao frontend or backend
- * 
+ *
  * @copyright inspiredminds 2015-2017
  * @package   facebook_js_sdk
  * @link      http://www.inspiredminds.at
@@ -12,6 +12,8 @@
  * @license   GPL-2.0
  */
 
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+use Contao\CoreBundle\DataContainer\PaletteNotFoundException;
 
 \System::loadLanguageFile('tl_settings');
 
@@ -19,8 +21,20 @@
 /**
  * Palettes
  */
-$GLOBALS['TL_DCA']['tl_page']['palettes']['root'].= ';{facebook_legend:hide},fb_app_id,fb_app_secret,fb_app_version,fb_sdk_frontend';
+(static function () {
+	$manipulator = PaletteManipulator::create()
+		->addLegend('facebook_legend', 'publish_legend', PaletteManipulator::POSITION_BEFORE, true)
+		->addField(
+			['fb_app_id', 'fb_app_secret', 'fb_app_version', 'fb_sdk_frontend'], 'facebook_legend',
+			PaletteManipulator::POSITION_APPEND
+		);
 
+	foreach (['root', 'rootfallback'] as $palette) {
+		try {
+			$manipulator->applyToPalette($palette, 'tl_page');
+		} catch (PaletteNotFoundException $e) {}
+	}
+})();
 
 /**
  * Fields
